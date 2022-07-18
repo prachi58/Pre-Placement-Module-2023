229679567d1b139ae093019133c4d958d68a7079
Prachi SIngh <prachisinghjatav58@gmail.com>
2022-07-18 19:06:48 +0530

    Create Symmetric_Tree.java

---
+++ DS I/Day 11/Symmetric_Tree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root.left,root.right);
+    }
+    private boolean check(TreeNode left,TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null || left.val != right.val) return false;
+        return (check(left.left,right.right) && check(left.right,right.left));
+    }
+}
