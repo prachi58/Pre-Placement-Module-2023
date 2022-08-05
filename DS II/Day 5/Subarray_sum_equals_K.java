30ecaebb1ca82e87b140b4e580625672999826d7
Prachi SIngh <prachisinghjatav58@gmail.com>
2022-08-05 19:08:59 +0530

    Create Subarray_sum_equals_K.java

---
+++ DS II/Day 5/Subarray_sum_equals_K.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0,c=0;
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++) {
+            sum+=nums[i];
+            int req=sum-k; //This is important please note and do not use Math.abs as it'll alter the solution
+            if(map.containsKey(req))
+                c+=map.get(req);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return c;
+    }
+}
